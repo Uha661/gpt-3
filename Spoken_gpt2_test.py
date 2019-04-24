6d56c8848daae38ec440d965f2a6f6ed7a173ff1
Naveen-Dodda <doddanaveenkrish@gmail.com>
2019-04-24 16:42:59 -0400

    test file

---
+++ Spoken_gpt2_test.py
@@ -0,0 +1,16 @@
+import fire
+import json
+import os
+import numpy as np
+import tensorflow as tf
+
+from interactive_conditional_samples import interact_model
+
+
+interact_model(
+    model_name='117M',
+    seed=None,
+    nsamples=1,batch_size=1,length=1,temperature=1,top_k=10,)
+
+
+
