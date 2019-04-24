312d24cc422ffbbd9a9ffc680702f62d3ae2f6ac
Naveen-Dodda <doddanaveenkrish@gmail.com>
2019-04-24 16:44:11 -0400

    test file

--- Spoken_gpt2_test.py
+++ Spoken_gpt2_test.py
@@ -10,7 +10,7 @@ from interactive_conditional_samples import interact_model
 interact_model(
     model_name='117M',
     seed=None,
-    nsamples=1,batch_size=1,length=1,temperature=1,top_k=10,)
+    nsamples=1,batch_size=1,length=1,temperature=1,top_k=10)
 
 
 
