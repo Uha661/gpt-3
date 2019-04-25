804e189c6a4acae73f35b24e35296742f5bdd098
Naveen-Dodda <doddanaveenkrish@gmail.com>
2019-04-25 13:33:16 -0400

    testing the chnages

--- Spoken_gpt2_test.py
+++ Spoken_gpt2_test.py
@@ -18,7 +18,7 @@ import tensorflow as tf
 import json
 import model, sample, encoder
 
-def interact_model(input_test_file,model_name='117M', seed=None, nsamples=1, batch_size=1, length=1, temperature=1, top_k=10, ):
+def interact_model(input_test_file='gpt-3_test_input.json',model_name='117M', seed=None, nsamples=1, batch_size=1, length=1, temperature=1, top_k=10, ):
     """
     Interactively run the model
     :model_name=117M : String, which model to use
