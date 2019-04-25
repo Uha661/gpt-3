4fe516c37d457f16cc7b1fe1b8d6b50281bb2583
Naveen-Dodda <doddanaveenkrish@gmail.com>
2019-04-25 14:34:23 -0400

    aded chnages

--- sample_spoken_edit.py
+++ sample_spoken_edit.py
@@ -50,10 +50,14 @@ def sample_sequence(*, hparams, length, start_token=None, batch_size=None, conte
             next_outputs = step(hparams, prev[:, tf.newaxis], past=past)
             logits = next_outputs['logits'][:, -1, :]  / tf.to_float(temperature)
             logits = top_k_logits(logits, k=top_k)
+            print(logits)
+            split1, split2 = tf.split(logits, num_or_size_splits=2, axis=0)
+            print(split1)
+            print(split2)
             #samples = tf.multinomial(logits, num_samples=1, output_dtype=tf.int32)
             return [
                 tf.concat([past, next_outputs['presents']], axis=-2),
-                tf.concat(logits,axis=1),
+                tf.concat(logits,axis=0),
                 tf.concat([output, logits], axis=1),
             ]
 
