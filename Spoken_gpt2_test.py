16d74d8a72f720279a87ea2e3ccd5010f9458490
Naveen-Dodda <doddanaveenkrish@gmail.com>
2019-04-26 12:01:07 -0400

    chnages testing

--- Spoken_gpt2_test.py
+++ Spoken_gpt2_test.py
@@ -72,10 +72,13 @@ def interact_model(input_test_file=None,model_name='117M', seed=None, nsamples=1
             context_tokens = enc.encode(raw_text)
             out = sess.run(output, feed_dict={context: [context_tokens for _ in range(batch_size)]})
             print(out[0])
-            top_10_predictions=enc.decode(out[0])
-            
-            for each_prediction_word in top_10_predictions.split(): 
-            	print(raw_text+' '+each_prediction_word+'\n')
+
+            for word in out[0]:
+            	wordarray = [ word ]
+            	print( raw_text+' ' + enc.decode(wordarray)+'\n' )
+
+            #for each_prediction_word in top_10_predictions.split(): 
+            #	print(raw_text+' '+each_prediction_word+'\n')
             	
                 #print((time.time() - start_time))
             print("=" * 80 )
