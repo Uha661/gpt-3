43bc0a4fa08ce2da3ccb6aefb4b3a9f31fbe2812
Naveen-Dodda <doddanaveenkrish@gmail.com>
2019-04-26 11:13:39 -0400

    testing chnages

--- Spoken_gpt2_test.py
+++ Spoken_gpt2_test.py
@@ -81,8 +81,8 @@ def interact_model(input_test_file=None,model_name='117M', seed=None, nsamples=1
 
                     text = enc.decode(out[i])
 
-                    for each_prediction in text.split(" "):
-                    	print(raw_text+each_prediction+'\n')
+                    for each_prediction in text.split():
+                    	print(raw_text+' '+each_prediction+'\n')
                     print("=" * 80 )
                 #print((time.time() - start_time))
 
